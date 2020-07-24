a923891841203b1c6a53f8c3061b5f8d4a4a89ed
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-24 18:32:10 +0530

    [Priyansh]Added:maximum amoung 3 floats by using CompareTo method

--- MaximumMethodApp/MaximumMethodTest/MaximumMethodUnitTest.cs
+++ MaximumMethodApp/MaximumMethodTest/MaximumMethodUnitTest.cs
@@ -8,17 +8,32 @@ namespace MaximumMethodTest
         [Test]
         public void givenNumbers_WhenTestedForMaximum_ShouldReturnFirstNumber()
         {
-            Assert.AreEqual(45, maximumMethod.findMaximum(45, 35, 40));
+            Assert.AreEqual(45, maximumMethod.findMaximumNumber(45, 35, 40));
         }
         [Test]
         public void givenNumbers_WhenTestedForMaximum_ShouldReturnSecondNumber()
         {
-            Assert.AreEqual(45, maximumMethod.findMaximum(40, 45, 35));
+            Assert.AreEqual(45, maximumMethod.findMaximumNumber(40, 45, 35));
         }
         [Test]
         public void givenNumbers_WhenTestedForMaximum_ShouldReturnThirdNumber()
         {
-            Assert.AreEqual(45, maximumMethod.findMaximum(40, 35, 45));
+            Assert.AreEqual(45, maximumMethod.findMaximumNumber(40, 35, 45));
+        }
+        [Test]
+        public void givenFloats_WhenTestedForMaximum_ShouldReturnFirstFloat()
+        {
+            Assert.AreEqual(23.9, maximumMethod.findMaximumFloat(23.9,3.8,2.9));
+        }
+        [Test]
+        public void givenFloats_WhenTestedForMaximum_ShouldReturnSecondFloat()
+        {
+            Assert.AreEqual(23.9, maximumMethod.findMaximumFloat(3.8,23.9, 2.9));
+        }
+        [Test]
+        public void givenFloats_WhenTestedForMaximum_ShouldReturnThirdFloat()
+        {
+            Assert.AreEqual(23.9, maximumMethod.findMaximumFloat(3.8,2.9,23.9));
         }
     }
 }
