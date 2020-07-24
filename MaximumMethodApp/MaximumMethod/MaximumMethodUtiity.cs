9fbfabeb33fe611a9696a630b8775c8ee199f9c5
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-24 18:15:49 +0530

    [Priyansh]Added:maximum amoung 3 integers by using CompareTo method

---
+++ MaximumMethodApp/MaximumMethod/MaximumMethodUtiity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaximumMethod
+{
+    public class MaximumMethodUtiity
+    {
+        public int findMaximum(int firstNumber,int secondNumber,int thirdNumber)
+        {
+            int maximum = firstNumber;
+            if (secondNumber.CompareTo(maximum) > 0)
+            {
+                maximum = secondNumber;
+            }
+            if (thirdNumber.CompareTo(maximum) > 0)
+            {
+                maximum = thirdNumber;
+            }
+            return maximum;
+        }
+    }
+}
